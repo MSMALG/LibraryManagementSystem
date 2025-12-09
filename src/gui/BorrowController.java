31249993cafc98273f4374960bd272573ba964ee
Muzna <91617542+MSMALG@users.noreply.github.com>
2025-12-09 23:10:55 +0300

    Update BorrowController.java
    Removed unused date formatters and updated date retrieval to use SQLITE_DB_FORMATTER.

--- src/gui/BorrowController.java
+++ src/gui/BorrowController.java
@@ -13,8 +13,7 @@ public class BorrowController {
     private static final int LOAN_DAYS = 14;
     private static final int LOAN_MINS = 2;
 
-    //private static final DateTimeFormatter SQLITE_FMT = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSS'Z'").withZone(ZoneOffset.UTC);
-    private static final DateTimeFormatter SQLITE_DB_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); // Removed .withZone(ZoneOffset.UTC)
+    private static final DateTimeFormatter SQLITE_DB_FORMATTER = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss"); 
 
     public boolean tryBorrow(String userEmail, int bookId) {
         try (Connection conn = DBConnection.connect()) {
@@ -42,19 +41,9 @@ public class BorrowController {
 
             //String today = LocalDate.now().format(FMT);
             //String due = LocalDate.now().plusDays(LOAN_DAYS).format(FMT);
-            //String due = ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(LOAN_MINS).format(FMT);
-            //String today = ZonedDateTime.now(ZoneOffset.UTC).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
-            //String due = ZonedDateTime.now(ZoneOffset.UTC).plusMinutes(LOAN_MINS).format(DateTimeFormatter.ISO_ZONED_DATE_TIME);
             String today = ZonedDateTime.now().format(SQLITE_DB_FORMATTER); 
             String due = ZonedDateTime.now().plusMinutes(LOAN_MINS).format(SQLITE_DB_FORMATTER);
-            /*try (PreparedStatement ps = conn.prepareStatement(
-                    "INSERT INTO loans(member_id, copy_id, loan_date, due_date) VALUES(?,?,?,?)")) {
-                ps.setInt(1, memberId);
-                ps.setInt(2, copyId);
-                ps.setString(3, today);
-                ps.setString(4, due);
-                ps.executeUpdate();
-            }*/
+
            try (PreparedStatement ps = conn.prepareStatement(
                 "INSERT INTO loans(member_id, copy_id, loan_date, due_date) VALUES(?,?,?,?)")) {
             ps.setInt(1, memberId);
